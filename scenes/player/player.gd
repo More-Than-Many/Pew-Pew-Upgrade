a92adf0190cbb89da7365a9953b490844b1c89bf
UltraKingLoser <jeremystanton97@gmail.com>
2025-10-20 20:01:55 +1000

    Project Creation
    Project Creation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,16 +3,16 @@ extends Sprite2D
 signal on_rotate
 
 var timer: float = 0.0
+var test_scale: int = 1
 
 func _ready():
+	test_scale = 2
+	self.scale = Vector2(test_scale, test_scale)
 	on_rotate.connect(rotateSprite)
 
 func rotateSprite() -> void:
 	rotate(PI/2)
 	
 func _process(delta: float) -> void:
-	timer += delta
-	if timer >= 3.0:
-		timer = 0
-		on_rotate.emit()
+	pass
 	
